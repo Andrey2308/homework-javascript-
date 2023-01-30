571219893bddc18abb0ba64a128de758a9aeeceb
Andrey <kadyska21@gmail.com>
2023-01-30 22:20:45 +0300

    homework_2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let celsius = 30;
+/*let celsius = 30;
 let farenheit = (9 / 5) * celsius + 32;
 console.log(farenheit);
 
 
 let nameUser = 'Andrey';
 let admin = nameUser;
-console.log(admin);
+console.log(admin);*/
