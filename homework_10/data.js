7192db7c3d1e655dcfd12deb25b2d1a83ea10bca
Andrey2308 <110895800+Andrey2308@users.noreply.github.com>
2023-02-25 12:59:37 +0300

    Add files via upload

---
+++ homework_10/data.js
@@ -0,0 +1,11 @@
+const dataCommander = `{
+	"url": "https://anapioficeandfire.com/api/characters/583",
+	"name": "Jon Snow",
+	"gender": "Male",
+	"culture": "Northmen",
+	"born": "In 283 AC",
+	"died": "no",
+	"titles": [
+		"Lord Commander of the Night's Watch"
+	]
+}`
