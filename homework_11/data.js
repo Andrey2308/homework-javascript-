961d9d222bdf5589776fe0b791f56e1884dfc184
Andrey2308 <110895800+Andrey2308@users.noreply.github.com>
2023-02-27 12:10:04 +0300

    Add files via upload

---
+++ homework_11/data.js
@@ -0,0 +1,45 @@
+// Дан макет сайта https://www.figma.com/file/mnLY69cYE5cqWM5w6n5hXx/Seo-%26-Digital-Marketing-Landing-Page?node-id=190%3A1194&t=q4NMnXTnwyyTSGA6-0
+      // В блоке Featured Items небходимо реализовать шаблон товаров.
+      // Для этого необходимо создать json формат данных по всем товарам.
+      // Из этого файла сформировать блок Featured Items.
+      // Всю вёрстку остальных частей реализовывать не нужно, если у вас она была сделана на html/css можно использовать, заново создавать не требуется.
+
+const dataBlock =  `[
+    {
+    "src":"/homework_11/images/1.jpg",
+    "name":"ELLERY X M'O CAPSULE",
+    "price": "$52.00",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
+    },
+    
+    {
+    "src":"/homework_11/images/2.jpg",
+    "name":"ELLERY X M'O CAPSULE",
+    "price": "$52.00",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
+    },
+    {
+    "src":"/homework_11/images/3.jpg",
+    "name":"ELLERY X M'O CAPSULE",
+    "price": "$52.00",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
+    },
+    {
+    "src":"/homework_11/images/4.jpg",
+    "name":"ELLERY X M'O CAPSULE",
+    "price": "$52.00",
+    "description": "Known for her sculptural takes on traditionaltailoring, Australian arbiter of cool Kym Ellery teams up withModa Operandi."
+    },
+    {
+    "src":"/homework_11/images/5.jpg",
+    "name":"ELLERY X M'O CAPSULE",
+    "price": "$52.00",
+    "description": "Known for her sculptural takes otraditional tailoring, Australian arbiter of cool KyEllery teams up with Moda Operandi."
+    },
+    {
+    "src":"/homework_11/images/6.jpg",
+    "name":"ELLERY X M'O CAPSULE",
+    "price": "$52.00",
+    "description": "Known for her sculptural takes traditional tailoring, Australian arbiter of cool KEllery teams up with Moda Operandi."
+    }
+]`
