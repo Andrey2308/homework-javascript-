0f9bb15dd25630060c270461a4a6a155ac148041
Andrey2308 <110895800+Andrey2308@users.noreply.github.com>
2023-03-05 01:48:57 +0300

    Add files via upload

---
+++ homework_12/data.js
@@ -0,0 +1,17 @@
+const dataVideo = `[
+    {
+        "name" : "rolik_1",
+        "src" : "https://mazwai.com/videvo_files/video/free/2016-01/small_watermarked/Forest_15_2_Videvo_preview.webm",
+        "description" : "природа"
+    },
+    {
+        "name" : "rolik_2",
+        "src" : "https://mazwai.com/videvo_files/video/free/2019-03/small_watermarked/190111_08_BuildingsTraffic_Drone_18_preview.webm?v=1477024",
+        "description" : "мегаполис"
+    },
+    {
+        "name" : "rolik_3",
+        "src" : "https://mazwai.com/videvo_files/video/free/2019-01/small_watermarked/181004_04_Dolphins-Whale_06_preview.webm?v=1409935",
+        "description" : "дельфины"
+    }
+    ]`
